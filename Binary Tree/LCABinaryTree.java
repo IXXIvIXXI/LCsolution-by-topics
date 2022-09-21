dfd6eb213f0a046b4807640823bac3adb5885019
IXXIvIXXI <88769471+IXXIvIXXI@users.noreply.github.com>
2022-09-21 15:54:52 -0700

    Create LCABinaryTree.java

---
+++ Binary Tree/LCABinaryTree.java
@@ -0,0 +1,19 @@
+// LC 236
+class Solution {
+    TreeNode res;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        findLCA(root, p, q);
+        return res;
+    }
+    
+    private boolean findLCA(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean left = findLCA(root.left, p, q);
+        boolean right = findLCA(root.right, p, q);
+        boolean curr = (root.val == p.val || root.val == q.val);
+        
+        if (left && right || left && curr || right && curr) res = root;
+        
+        return left || right || curr; 
+    }
+}
